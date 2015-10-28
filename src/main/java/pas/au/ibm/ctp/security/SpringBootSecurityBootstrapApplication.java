5c2f3afc84ef52a5c020f1f4acc53e346dd05f3f
pasapicella <pas.apicella@tpg.com.au>
2015-10-28 15:37:13 -0700

    Initial commit

---
+++ src/main/java/pas/au/ibm/ctp/security/SpringBootSecurityBootstrapApplication.java
@@ -0,0 +1,12 @@
+package pas.au.ibm.ctp.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityBootstrapApplication.class, args);
+    }
+}
